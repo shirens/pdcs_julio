e26da589b874d6b62dc62da1200d90457015f359
Prita Lauren <2522500076@mahasiswa.atmaluhur.ac.id>
2026-09-18 18:04:59 +0700

    tamh picture

---
+++ mahasiswa.php
@@ -0,0 +1,13 @@
+{
+  "status": "success",
+  "message": "Data mahasiswa berhasil diambil",
+  "data": {
+    "id": 1,
+    "nim": "2511500076",
+    "nama": "Julio Putrawan",
+    "email": "2511500076@mahasiswa.atmaluhur.ac.id",
+    "jurusan": "Teknik Informatika",
+    "semester": 3,
+    "ipk": 3.75
+  }
+}
